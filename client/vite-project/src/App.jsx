1e2651271c72f9f8208ee2be4093d3137411801a
Prahas P B Rao <prahaspbrao2005@gmail.com>
2026-01-19 21:30:11 +0530

    completed the project

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -21,7 +21,20 @@ function App() {
   if (loading) return <h1>Loading...</h1>;
   if (error) return <h1>Error: {error.message}</h1>;
 
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  return <div className="App">
+     <table>
+      <tbody>
+        {
+          data.getTodos.map((todo) => (
+            <tr key={todo.id}>
+              <td>{todo.title}</td>
+              <td>{todo?.user?.name}</td>
+            </tr>
+          ))
+        }
+      </tbody>
+     </table>
+  </div>
 }
 
 export default App;
